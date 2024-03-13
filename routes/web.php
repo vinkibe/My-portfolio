3f22ae51bad22bd06119e1b1d8da266de43af4fe
Vincent Kibet <128219446+vinkibe@users.noreply.github.com>
2024-03-13 14:30:45 +0300

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [ContactController::class, 'index'])->name('contact.index');
+
+// To Send data to email.
+Route::post('/', [ContactController::class, 'store'])->name('contact.store');
---
+++ storage/framework/views/4b585f0487046b3dbfa5a51bb60a6a59.php
@@ -0,0 +1,8 @@
+<p>Message:</p>
+<p><?php echo e($data['message']); ?></p>
+
+<div style="margin-top: 50px;">
+<p>Name: <?php echo e($data['name']); ?> </p>
+<p>Email: <?php echo e($data['email']); ?></p>
+</div>
+<?php /**PATH /home/vin/Desktop/workspace/Laravel-Portfolio/resources/views/mail/mail.blade.php ENDPATH**/ ?>
